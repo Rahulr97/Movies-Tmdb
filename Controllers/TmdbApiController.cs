0fa8dd31b068ff487d0ee659859223ca550274a2
Rahul Rubugunday <rahulrubugunday@Rahuls-MacBook-Pro.local>
2021-02-08 10:49:17 +0530

    initial commit

---
+++ Controllers/TmdbApiController.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Net;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using piquota.Models;
+using System.Text;
+
+namespace piquota.Controllers
+{
+   
+    public class TmdbApiController : Controller
+    {
+        
+        public void CallAPI(string searchText, int PageNo)
+        {
+            string url = "https://api.themoviedb.org/3/search/movie";
+            string apikey = "107c1d9499aab019dd11605773c06ac2";
+            HttpWebRequest getRequest = WebRequest.Create(url + "?api_key=" + apikey + "&language=en-US&query=" + searchText) as HttpWebRequest;
+            string apiResponse = "";
+            
+            using (HttpWebResponse response = getRequest.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                apiResponse = reader.ReadToEnd();
+            }
+
+            ResponseSearchMovie rootObject = JsonConvert.DeserializeObject<ResponseSearchMovie>(apiResponse);
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("<div class=\"resultDiv\"><p>Names</p>");
+            foreach (Result result in rootObject.results)
+            {
+                string image = result.profile_path == null ? Url.Content("~/Content/Image/no-image.png") : "https://image.tmdb.org/t/p/w500/" + result.profile_path;
+                string link = Url.Action("GetMovie", "TmdbApi", new { id = result.id });
+
+                sb.Append("<div class=\"result\" resourceId=\"" + result.id + "\">" + "<a href=\"" + link + "\"><img src=\"" + image + "\" />" + "<p>" + result.name + "</a></p></div>");
+            }
+
+            ViewBag.Result = "Test";
+            ViewData["Result"] = "Test";
+            
+            int pageSize = 20;
+            PagingInfo pagingInfo = new PagingInfo();
+            pagingInfo.CurrentPage = PageNo;
+            pagingInfo.TotalItems = rootObject.total_results;
+            pagingInfo.ItemsPerPage = pageSize;
+            ViewBag.Paging = pagingInfo;
+        }
+
+
+        public ActionResult Index(string Movie, int PageNo)
+        {
+            //CallAPI(Movie, PageNo);
+            //Models.Movie theMovieDb = new Models.Movie();
+            //theMovieDb.searchText = Movie;
+            ViewBag.Result1 = "Test1";
+            ViewData["Result1"] = "Test1";
+            return View();
+        }
+        //[HttpPost]
+        //public ActionResult Index(Models.Movie theMovieDb, string searchText)
+        //{
+
+        //    //if (ModelState.IsValid)
+        //    //{
+        //    //    CallAPI(searchText,0);
+        //    //}
+        //    //return View(theMovieDb);
+        //}
+
+
+        public ActionResult GetMovie(int id)
+        {
+            /*Calling API https://developers.themoviedb.org/3/movie */
+            string apiKey = "3356865d41894a2fa9bfa84b2b5f59bb";
+            HttpWebRequest apiRequest = WebRequest.Create("https://api.themoviedb.org/3/movie/" + id + "?api_key=" + apiKey + "&language=en-US") as HttpWebRequest;
+
+            string apiResponse = "";
+            using (HttpWebResponse response = apiRequest.GetResponse() as HttpWebResponse)
+            {
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                apiResponse = reader.ReadToEnd();
+            }
+            /*End*/
+
+            /*http://json2csharp.com*/
+            ResponseMovie rootObject = JsonConvert.DeserializeObject<ResponseMovie>(apiResponse);
+            Movie theMovieDb = new Movie();
+            theMovieDb.title = rootObject.title;
+            theMovieDb.overview = rootObject.overview;
+            theMovieDb.production_companies = rootObject.production_companies;
+            theMovieDb.release_date = rootObject.release_date;
+            theMovieDb.imdb_id = rootObject.imdb_id;
+            theMovieDb.runtime = rootObject.runtime;
+            theMovieDb.production_companies = rootObject.production_companies;
+            
+
+            return View(theMovieDb);
+        }
+    }
+
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace piquota.Models
+{
+    public class Movie
+    {   
+            public string searchText { get; set; }
+            public bool adult { get; set; }
+            public string id { get; set; }
+            public string imdb_id { get; set; }
+            public string title { get; set; }
+            public string overview { get; set; }
+            public string poster_path { get; set; }
+            public string homepage { get; set; }
+            public string production_companies { get; set; }
+            public string release_date { get; set; }
+            public string revenue { get; set; }
+            public double vote_average { get; set; }
+            public double popularity { get; set; }
+            public int runtime { get; set; }
+       
+    }
+    public class Result
+    {
+        public string profile_path { get; set; }
+        public bool adult { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public double popularity { get; set; }
+    }
+    public class ResponseSearchMovie
+    {
+        public int page { get; set; }
+        public List<Result> results { get; set; }
+        public int total_results { get; set; }
+        public int total_pages { get; set; }
+    }
+
+    public class ResponseMovie
+    {
+        public bool adult { get; set; }
+        public string id { get; set; }
+        public string title { get; set; }
+        public string overview { get; set; }
+        public string production_companies { get; set; }
+        public string original_language { get; set; }
+        public string status { get; set; }
+        public string release_date { get; set; }
+        public string revenue { get; set; }
+        public string imdb_id { get; set; }
+        public string name { get; set; }
+        public string vote_average { get; set; }
+        public double popularity { get; set; }
+        public int runtime { get; set; }
+    }
+    public class PagingInfo
+    {
+        public int TotalItems { get; set; }
+        public int ItemsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        public int TotalPages
+        {
+            get
+            {
+                return (int)Math.Ceiling((decimal)TotalItems /
+                    ItemsPerPage);
+            }
+        }
+    }
+}
